0fcba5086b6c1c86659483b80f49e5a9da58c6da
Anas Saidi <anassaidi1337@gmail.com>
2022-07-22 15:17:13 +0100

    Saving to localStorage

--- script.js
+++ script.js
@@ -2,13 +2,28 @@ const adviceCard = document.querySelector('.advice-card');
 const adviceId = document.querySelector('.advice_id');
 const adviceText= document.querySelector('.advice-text');
 const adviceButton = document.querySelector('.advice_button');
+const saveButton = document.querySelector('.save_button');
+const saveIcon= document.querySelector('.save_icon');
 firstAdvice();
 adviceButton.addEventListener('click',getAdvice);
+if(localStorage.getItem('saved')===null){
+let savedlist = []
+localStorage.setItem('saved',JSON.stringify(savedlist));
+}
+
 function getAdvice(){
     adviceCard.classList.add('shake_it');
     const url = 'https://api.adviceslip.com/advice';
     fetch(url).then(res=>res.json()).then(data=>{
         adviceCard.addEventListener('animationend',()=>{
+        if(!JSON.parse(localStorage.getItem("saved")).includes(data.slip.id)){
+            saveIcon.classList.add('unsaved');
+            saveIcon.classList.remove('saved');
+        }
+        else {
+            saveIcon.classList.add('saved');
+            saveIcon.classList.remove('unsaved'); 
+        }
         adviceId.innerText = `ADVICE #${data.slip.id}`;
         adviceText.innerText = data.slip.advice;
         adviceCard.classList.remove('shake_it');
@@ -24,3 +39,23 @@ function firstAdvice(){
         adviceText.innerText = data.slip.advice;
     }).catch(err=>{ console.log(err); });
 }
+function saveAdvice(){
+    saveIcon.classList.toggle('saved')
+    saveIcon.classList.toggle('unsaved') 
+    if(saveIcon.classList.contains('saved')){
+        console.log("Added to list")
+    let arr=JSON.parse(localStorage.getItem('saved'))
+    arr.push(Number(adviceId.innerText.split('#')[1]))
+    localStorage.setItem('saved',JSON.stringify(arr))
+    }
+    else {
+        let arr=JSON.parse(localStorage.getItem('saved'))
+        arr.pop(adviceId.innerText.split('#')[1])
+        localStorage.setItem('saved',JSON.stringify(arr))
+        console.log('Remove from list')
+    }
+
+    
+        
+}
+saveButton.addEventListener('click',saveAdvice)
